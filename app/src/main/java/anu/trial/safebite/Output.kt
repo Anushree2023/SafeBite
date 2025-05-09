9f5b5214568b1c3f72c95d5934324e456b690343
Anushree2023 <anushreethosar@gmail.com>
2025-05-09 20:43:05 +0530

    UUUUUUFFFF UI

--- app/src/main/java/anu/trial/safebite/Output.kt
+++ app/src/main/java/anu/trial/safebite/Output.kt
@@ -4,13 +4,14 @@ package anu.trial.safebite
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Output : AppCompatActivity() {
 
     private lateinit var extractedTextView: TextView
-    private lateinit var backButton: Button
+    private lateinit var backButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
